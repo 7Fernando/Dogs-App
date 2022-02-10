29fcb2ea7d5e58d9de2e527507048ac482540e52
Your Name <Luiscabezas_@outlook.com>
2022-02-09 22:22:44 -0300

    css

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -9,6 +9,8 @@ import { useEffect, useState } from "react";
 import Filters from "../Filters/Filters";
 import { Link } from "react-router-dom";
 import SearchBar from "../SearchBar/SearchBar";
+import { AiFillHome } from "react-icons/ai";
+
 const Home = () => {
   //const [loading, setLoading] = useState(false);
   const [currentPage, setCurrentPage] = useState(1);
@@ -51,21 +53,26 @@ const Home = () => {
     return (
       <div>
         <ul className={styles.ul1}>
-        <li className={styles.li}>
+          <li>
+            <AiFillHome className={styles.home2} onClick={refreshPage} />
+          </li>
+          
+            
+         
+
+          <li className={styles.li}>
             <SearchBar />
           </li>
-          <li  className={styles.li}>
+          <li className={styles.li}>
             <Link to="/dog">
               <button className={styles.btn}>Create a new dog!</button>
             </Link>
           </li>
         </ul>
         <ul className={styles.menuUl}>
-          
-          <li  className={styles.li}>
+          <li className={styles.li}>
             <Filters />
           </li>
-         
         </ul>
 
         <Pagination
